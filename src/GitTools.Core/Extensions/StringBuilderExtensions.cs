22437c261d98bb77e301869f577ac0fcbd0ffda5
Geert van Horrik <geert@catenalogic.com>
2015-04-20 17:55:46 +0200

    Added JetBrainsAnnotations.Fody

--- src/GitTools.Core/Extensions/StringBuilderExtensions.cs
+++ src/GitTools.Core/Extensions/StringBuilderExtensions.cs
@@ -1,10 +1,11 @@
 ﻿namespace GitTools
 {
     using System.Text;
+    using JetBrains.Annotations;
 
     public static class StringBuilderExtensions
     {
-        //[StringFormatMethod("format")]
+        [StringFormatMethod("format")]
         public static void AppendLineFormat(this StringBuilder stringBuilder, string format, params object[] args)
         {
             stringBuilder.AppendFormat(format, args);
