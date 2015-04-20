25d0a38e367b0b9a81e9c04e16c1446715f269d9
Geert van Horrik <geert@catenalogic.com>
2015-04-20 17:27:30 +0200

    Added more extension methods (with some unit tests)

---
+++ src/GitTools.Core.Tests/Extensions/StringExtensionsFacts.cs
@@ -0,0 +1,20 @@
+﻿namespace GitTools.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringExtensionsFacts
+    {
+        [TestCase("/develop", false)]
+        [TestCase("/master", false)]
+        [TestCase("/pr/25", true)]
+        [TestCase("/pull/25", true)]
+        [TestCase("/pull-requests/25", true)]
+        public void TheIsPullRequestMethod(string input, bool expectedValue)
+        {
+            var actualValue = input.IsPullRequest();
+            
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
