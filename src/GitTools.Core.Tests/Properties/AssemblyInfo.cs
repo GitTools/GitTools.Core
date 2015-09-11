431f2dd7226b7ccd7eb19ae4b5fc5c155dbc1eff
Jake Ginnivan <jake@ginnivan.net>
2015-09-11 18:12:21 +0800

    Go back to packages folder

--- src/GitTools.Core.Tests/Properties/AssemblyInfo.cs
+++ src/GitTools.Core.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,9 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="GitTools">
-//   Copyright (c) 2014 - 2015 GitTools. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // All other assembly info is defined in SharedAssembly.cs
 
 [assembly: AssemblyTitle("GitTools.Core.Tests")]
-[assembly: AssemblyProduct("GitTools.Core.Tests")]
 [assembly: AssemblyDescription("GitTools.Core.Tests library")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
