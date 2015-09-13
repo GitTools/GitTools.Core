4791fc7ef7d671a2ecc823fa591c619a41f74742
Jake Ginnivan <jake@ginnivan.net>
2015-09-13 13:42:54 +0800

    Project updates

--- src/GitTools.IssueTrackers/Properties/AssemblyInfo.cs
+++ src/GitTools.IssueTrackers/Properties/AssemblyInfo.cs
@@ -1,20 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="GitTools">
-//   Copyright (c) 2014 - 2015 GitTools. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-// All other assembly info is defined in SharedAssembly.cs
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("GitTools.IssueTrackers")]
 [assembly: AssemblyDescription("GitTools.IssueTrackers library")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
